8a7ab3342aaa9b829042ed1a7cf21ab7e5b41c69
karlacorrea <47987828+karlacorrea@users.noreply.github.com>
2021-08-19 23:32:35 -0300

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://<username>:<password>@cluster0.2zoqj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
